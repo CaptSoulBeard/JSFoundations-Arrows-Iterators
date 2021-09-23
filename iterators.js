3e1c56834f5c884b0762c05ebd152625dd5c075b
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-09-23 14:40:00 +0300

    Partially solved the Arrows task

--- iterators.js
+++ iterators.js
@@ -5,8 +5,10 @@
  * - Logs every element of the array
  ************************************/
 const logger = function (array) {
-
+  array.forEach((element) => console.log(element));
 };
+
+logger([1, 2, 3, 4, 5, 6, 7]);
 
 /*************************************
  * toCelsius(temperatures)
@@ -19,9 +21,14 @@ const logger = function (array) {
  * The conversion is:
  *   C = (F - 32) * (5/9)
  ************************************/
-const toCelsius = function (temperatures) {
 
+const toCelsius = function (temperatures) {
+  return temperatures.map((F) => {
+    return (F - 32) * (5 / 9);
+  });
 };
+
+console.log(toCelsius([212, 122])); // should be: [100, 50];
 
 /**************************************
  * hottestDays(temperatures, threshhold)
@@ -31,9 +38,15 @@ const toCelsius = function (temperatures) {
  * - Returns an array of temperatures
  *   that exceed the threshhold
  ***************************************/
-const hottestDays = function (temperatures, threshhold) {
 
+const hottestDays = function (temperatures, threshhold) {
+  const filteredTemperatures = temperatures.filter((element) => {
+    return element > threshhold;
+  });
+  return filteredTemperatures;
 };
+
+console.log(hottestDays([1, 2, 3, 4, 5, 6, 7, 8, 13, 156, 1765], 5)); // should be : [6, 7, 8, 13, 156, 1765]
 
 /******************************************
  * logHottestDays(temperatures, threshhold)
@@ -49,9 +62,9 @@ const hottestDays = function (temperatures, threshhold) {
  * hint: you can combine
  *       all previous functions
  *******************************************/
-const logHottestDays = function (temperatures, threshhold) {
+const logHottestDays = function (temperatures, threshhold) {};
 
-};
+logHottestDays([140, 68, 212, 45, 149, 122, 19], 80); // should log "60", "100", "65", and "50"
 
 /* Uncomment the following lines to test your code... */
 
